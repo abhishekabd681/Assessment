9cffb5712607fc3ab753c97acf988fd53654fff6
Abhishek <abhishekabd681@gmail.com>
2022-11-01 22:50:06 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './Component/Navigation/Navigation';
+import Header from './Component/Header/Header';
+import Product from './Component/Product/Product';
+import Footer from './Component/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Navigation />
+       <Header />
+       <Product />
+       <Footer />
     </div>
   );
 }
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import './Header.css'
+import main from './main.png'
+
+function Header() {
+  return (
+    <div>
+
+        <div className="main1">
+            <img id='bigImg' src={main} alt="" />
+        </div>
+
+        
+
+
+      
+    </div>
+  )
+}
+
+export default Header
